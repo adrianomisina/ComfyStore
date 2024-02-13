6578777a1ab86cdc62096c57eb5ce5a251334834
adrianomisina <adriano.misina@gmail.com>
2024-02-13 10:11:59 -0300

    Add to cart functionality

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -3,6 +3,8 @@ import { useLoaderData } from 'react-router-dom';
 import { formatPrice, customFetch, generateAmountOptions } from '../utils';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addItem } from '../features/cart/cartSlice';
 
 export const loader = async ({ params }) => {
   const response = await customFetch(`/products/${params.id}`)
@@ -20,6 +22,23 @@ const SingleProduct = () => {
 
   const handleAmount = (e) => {
     setAmount(parseInt(e.target.value))
+  }
+
+  const cartProduct = {
+    cartID: product.id + productColor,
+    productID: product.id,
+    image: image,
+    title: title,
+    price: price,
+    description: description,
+    colors: colors,
+    company: company
+  }
+
+  const dispatch = useDispatch();
+
+  const addToCart = () => {
+    dispatch(addItem({product:cartProduct}))
   }
 
   return (
@@ -90,7 +109,7 @@ const SingleProduct = () => {
           <div className="mt-10">
             <button
               className='btn btn-secondary btn-md uppercase'
-              onClick={() => console.log('add to bag')}
+              onClick={addToCart}
             >
               Add to bag
             </button>
